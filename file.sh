dce33d9d010bd10bfc9ea25ee260c7636d0dbe5e
AtabikM1 <mutawakilatabik@gmail.com>
2024-05-13 15:58:01 +0700

    update

---
+++ file.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Program sederhana untuk menyapa pengguna
+
+# Meminta pengguna untuk memasukkan namanya
+echo "Selamat datang!"
+echo -n "Siapa nama Anda? "
+read nama
+
+# Menyapa pengguna berdasarkan nama yang dimasukkan
+echo "Halo, $nama! Selamat datang di program ini."
